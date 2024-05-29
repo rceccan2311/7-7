bf4e8950ece10af0e529746e4606a80dcadb45bc
rceccan2311 <132548157+rceccan2311@users.noreply.github.com>
2024-05-29 08:48:59 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+let distancia = 0
+basic.forever(function () {
+    distancia = maqueen.Ultrasonic(PingUnit.Centimeters)
+    if (distancia > 0 && distancia < 10) {
+        maqueen.motorStop(maqueen.Motors.All)
+    } else {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 25)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 25)
+    }
+})
